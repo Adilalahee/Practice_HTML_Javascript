eddf866b6a98826b2f5daa177a631d00d3e3e95f
Adil Alahee <adilmdctg@gmail.com>
2024-09-20 07:38:39 -0700

    DOM Basic

---
+++ Js/style.js
@@ -0,0 +1,10 @@
+console.log('Welcome to DOM world');
+console.log(document);
+const student={
+    name:'Adil',
+    roll:56,
+    read: function(study){
+        console.log(study,'Academic')
+    }
+};
+document.getElementsByTagName('h1');
